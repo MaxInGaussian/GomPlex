bb3e889a3470c9e015bbc6097ade892ef6451639
Max W. Y. Lam <maxingaussian@gmail.com>
2017-05-02 19:01:14 +0800

    commit from max-OptiPlex-7040 by max

--- GomPlex/util/Linalg.py
+++ GomPlex/util/Linalg.py
@@ -1,22 +1,95 @@
 ################################################################################
-#  Github: https://github.com/MaxInGaussian/GomPlex
-#  Author: Max W. Y. Lam (maxingaussian@gmail.com)
+#  Github:https://github.com/MaxInGaussian/GomPlex
+#  Author:Max W. Y. Lam (maxingaussian@gmail.com)
 ################################################################################
 
 import numpy as np
 from scipy import linalg
+from scipy.sparse import csr_matrix
+from numpy.fft import fft, ifft, fftshift, ifftshift
 
 _all__ = [
-    "approx_Phi",
-    "interp_Phi",
+    "ndft",
+    "nfft",
+    "interp_Phi_by_basis",
+    "get_Phi_by_basis",
+    "interp_Phi_by_FFT",
+    "get_Phi_by_FFT",
     "fast_multiply",
     "fast_solve"
 ]
 
-def approx_Phi(W, Phi_basis):
-    return fast_multiply(Phi_basis, W.conj().T, True).conj().T
+def ndft(x, f_hat, M):
+    k = -(M//2)+np.arange(M)
+    return np.dot(np.exp(-2j*np.pi*k*x[:, None]), f_hat)
+    
+def nfft(x, f_hat, M, sigma=2, tol=1e-8):
+    n = M*sigma
+    m = np.ceil(-np.log(0.25*tol/M)/(np.pi*(1-1/(2*sigma-1))))
+    shift = lambda x:-0.5+(x+0.5)%1
+    k = -(M//2)+np.arange(M)
+    b = (2*sigma*m)/((2*sigma-1)*np.pi)
+    g_hat = f_hat/np.exp(-b*(np.pi*k/n)**2)
+    g_hat_n = np.concatenate([g_hat[M//2:], np.zeros(n-M), g_hat[:M//2]])
+    g = fftshift(fft(g_hat_n))
+    col_ind = np.floor(n*x[:, np.newaxis]).astype(int)+np.arange(-m, m)
+    vals = np.exp(-(n*shift(x[:, None]-col_ind/n))**2/b)/np.sqrt(np.pi*b)
+    col_ind = (col_ind+n//2)%n
+    indptr = np.arange(len(x)+1)*col_ind.shape[1]
+    mat = csr_matrix((vals.ravel(), col_ind.ravel(), indptr), shape=(len(x), n))
+    f = mat.dot(g)
+    return f
 
-def interp_Phi(Phi, phi_basis):
+def ndft_mat(x, F_hat, M):
+    k = -(M//2)+np.arange(M)
+    return np.dot(np.exp(-2j*np.pi*k*x[:, None]), F_hat)
+    
+def nfft_mat(x, F_hat, M, sigma=2, tol=1e-8):
+    F = np.zeros((x.shape[0], F_hat.shape[1]))+0j
+    for j in range(F_hat.shape[1]):
+        F[:, j] = nfft(x, F_hat[:, j], M, sigma, tol)
+    return F
+
+def adj_ndft(x, f, M):
+    k = -(M//2)+np.arange(M)
+    return np.dot(np.exp(2j*np.pi*x*k[:, None]), f)
+    
+def adj_nfft(x, f, M, sigma=2, tol=1e-8):
+    n = M*sigma
+    m = np.ceil(-np.log(0.25*tol/M)/(np.pi*(1-1/(2*sigma-1))))
+    shift = lambda x:-0.5+(x+0.5)%1
+    col_ind = np.floor(n*x[:, None]).astype(int)+np.arange(-m, m)
+    b = (2*sigma*m)/((2*sigma-1)*np.pi)
+    vals = np.exp(-(n*shift(x[:, None]-col_ind/n))**2/b)/np.sqrt(np.pi*b)
+    col_ind = (col_ind+n//2)%n
+    indptr = np.arange(len(x)+1)*col_ind.shape[1]
+    mat = csr_matrix((vals.ravel(), col_ind.ravel(), indptr), shape=(len(x), n))
+    g = mat.T.dot(f)
+    k = -(M//2)+np.arange(M)
+    g_hat_n = fftshift(ifft(ifftshift(g)))
+    g_hat = n*g_hat_n[(n-M)//2:(n+M)//2]
+    f_hat = g_hat/np.exp(-b*(np.pi*k/n)**2)
+    return f_hat
+
+def adj_ndft_mat(x, F, M):
+    k = -(M//2)+np.arange(M)
+    return np.dot(np.exp(2j*np.pi*x*k[:, None]), F)
+    
+def adj_nfft_mat(x, F, M, sigma=2, tol=1e-8):
+    F_hat = np.zeros((x.shape[0], F.shape[1]))+0j
+    for j in range(F.shape[1]):
+        F_hat[:, j] = adj_nfft(x, F[:, j], M, sigma, tol)
+    return F_hat
+
+def Phi(X, spectral_freqs):
+    X_sparse = self.X.dot(self.spectral_freqs)
+    Phi_const = np.sqrt(self.kernel_scale/self.M)
+    Phi = Phi_const*np.exp(2j*np.pi*X_sparse)
+    return Phi
+
+def Phi_nfft(X, spectral_freqs):
+    X_sparse = X.dot(spectral_freqs)
+    
     W_H = np.zeros((phi_basis.shape[0], Phi.shape[0]))+0j
     H_basis = np.concatenate(([phi_basis[0]], phi_basis[1:][::-1])).conj()
     fft_basis = np.fft.fft(H_basis)
@@ -25,23 +98,29 @@ def interp_Phi(Phi, phi_basis):
         W_H[:, i] = np.fft.ifft(np.fft.fft(Phi[i, :].conj())/fft_basis)
     return W_H.conj().T
 
-def interp_Phi_by_FFT(Phi, type=1):
-    W = np.zeros_like(Phi)+0j
-    for j in range(Phi.shape[type]):
-        if(type == 1):
-            W[:, j] = np.fft.fft(Phi[:, j])
-        elif(type == 0):
-            W[j, :] = np.fft.fft(Phi[j, :])
-    return W
+def interp_Phi_by_basis(Phi, phi_basis):
+    W_H = np.zeros((phi_basis.shape[0], Phi.shape[0]))+0j
+    H_basis = np.concatenate(([phi_basis[0]], phi_basis[1:][::-1])).conj()
+    fft_basis = np.fft.fft(H_basis)
+    Phi_basis = linalg.circulant(phi_basis)
+    for i in range(Phi.shape[0]):
+        W_H[:, i] = np.fft.ifft(np.fft.fft(Phi[i, :].conj())/fft_basis)
+    return W_H.conj().T
+
+def get_Phi_by_basis(W, Phi_basis):
+    return fast_multiply(Phi_basis, W.conj().T, True).conj().T
+
+def interp_Phi_by_FFT(Phi, vert=True):
+    if(vert):
+        return np.fft.fft(Phi.T).T
+    else:
+        return np.fft.fft(Phi)
     
-def interp_Phi_by_IFFT(W, type=1):
-    Phi = np.zeros_like(W)+0j
-    for j in range(W.shape[type]):
-        if(type == 1):
-            Phi[:, j] = np.fft.ifft(W[:, j])
-        elif(type == 0):
-            Phi[j, :] = np.fft.ifft(W[j, :])
-    return Phi
+def get_Phi_by_FFT(W, vert=True):
+    if(vert):
+        return np.fft.ifft(W.T).T
+    else:
+        return np.fft.ifft(W)
     
 def fast_multiply(C, X, conj_trans=False):
     CX = np.zeros((C.shape[0], X.shape[1]))+0j
--- tests/test_Linalg.py
+++ tests/test_Linalg.py
@@ -10,44 +10,78 @@ from scipy import linalg
 from GomPlex import *
 
 N, M, D = 500, 100, 30
-X = .5*np.random.rand(N, D)+.5j*np.random.rand(N, D)
+X = .5*np.random.rand(N, D)
 C = linalg.circulant(np.random.rand(N)+1j*np.random.rand(N))
+k = np.arange(M)+1
+Omega = np.random.randn(D, M)
+X_O = X.dot(Omega)
+X_nfft = X_O/k[None, :]
+x = X_nfft[0, :]
+f = np.sin(-20*np.pi*x)
+Phi = np.exp(-2j*np.pi*X_O)/np.sqrt(M)
+alpha = np.random.randn(M, M)+1j*np.random.randn(M, M)
 
+print()
+print('test of nfft')
+print('approx l0 error:',
+    np.max(np.abs(ndft(x, f, M)-nfft(x, f, M)))/np.mean(np.absolute(f)))
+print('approx l1 error:',
+    np.mean(np.abs(ndft(x, f, M)-nfft(x, f, M)))/np.mean(np.absolute(f)))
+
+print()
+print('test of nfft_mat')
+print('approx l0 error:',
+    np.max(np.abs(ndft_mat(x, alpha, M)-nfft_mat(x, alpha, M)))/np.mean(np.absolute(alpha)))
+print('approx l1 error:',
+    np.mean(np.abs(ndft_mat(x, alpha, M)-nfft_mat(x, alpha, M)))/np.mean(np.absolute(alpha)))
+Y = ndft_mat(x, alpha, M)
+    
+print()
+print('test of adj_nfft')
+print('approx l0 error:',
+    np.max(np.abs(adj_ndft(x, f, M)-adj_nfft(x, f, M)))/np.mean(np.absolute(f)))
+print('approx l1 error:',
+    np.mean(np.abs(adj_ndft(x, f, M)-adj_nfft(x, f, M)))/np.mean(np.absolute(f)))
+
+print()
+print('test of adj_nfft_mat')
+print('approx l0 error:',
+    np.max(np.abs(adj_ndft_mat(x, Y, M)-adj_nfft_mat(x, Y, M)))/np.mean(np.absolute(Y)))
+print('approx l1 error:',
+    np.mean(np.abs(adj_ndft_mat(x, Y, M)-adj_nfft_mat(x, Y, M)))/np.mean(np.absolute(Y)))
+
+print()
 print('test of fast_multiply')
-print('all close:', np.allclose(C.dot(X), fast_multiply(C, X)))
+print('approx l0 error:',
+    np.max(np.abs(C.dot(X)-fast_multiply(C, X))))
+print('approx l1 error:',
+    np.mean(np.abs(C.dot(X)-fast_multiply(C, X))))
 
 print()
 print('test of fast_multiply with conjugate transpose')
-print('all close:', np.allclose(C.conj().T.dot(X), fast_multiply(C, X, True)))
+print('approx l0 error:',
+    np.max(np.abs(C.conj().T.dot(X)-fast_multiply(C, X, True))))
+print('approx l1 error:',
+    np.mean(np.abs(C.conj().T.dot(X)-fast_multiply(C, X, True))))
 
 print()
-print('test of interp_Phi')
-Omega = np.random.randn(D, M)
-X_O = X.dot(Omega)
-Phi = np.exp(-2j*np.pi*X_O)/np.sqrt(M)
+print('test of interp_Phi_by_basis')
 phi_basis = np.exp(-2j*np.pi*np.median(X_O, 0))/np.sqrt(M)
 Phi_basis = linalg.circulant(phi_basis)
-W = interp_Phi(Phi, phi_basis)
+W = interp_Phi_by_basis(Phi, phi_basis)
 W_true = (linalg.solve(Phi_basis.T, Phi.T)).T
 print('approx l0 error:',
-    np.max(np.abs(np.dot(W, Phi_basis)-Phi))/np.mean(np.absolute(Phi)))
+    np.max(np.abs(get_Phi_by_basis(W, Phi_basis)-Phi))/np.mean(np.absolute(Phi)))
 print('approx l1 error:',
-    np.mean(np.abs(np.dot(W, Phi_basis)-Phi))/np.mean(np.absolute(Phi)))
+    np.mean(np.abs(get_Phi_by_basis(W, Phi_basis)-Phi))/np.mean(np.absolute(Phi)))
     
 print()
 print('test of interp_Phi_by_FFT')
 W_fft = interp_Phi_by_FFT(Phi)
 print('approx l0 error:',
-    np.max(np.abs(interp_Phi_by_IFFT(W_fft)-Phi))/np.mean(np.absolute(Phi)))
+    np.max(np.abs(get_Phi_by_FFT(W_fft)-Phi))/np.mean(np.absolute(Phi)))
 print('approx l1 error:',
-    np.mean(np.abs(interp_Phi_by_IFFT(W_fft)-Phi))/np.mean(np.absolute(Phi)))
-
-print()
-print('test of approx_Phi')
-print('approx l0 error:',
-    np.max(np.abs(approx_Phi(W, Phi_basis)-Phi))/np.mean(np.absolute(Phi)))
-print('approx l1 error:',
-    np.mean(np.abs(approx_Phi(W, Phi_basis)-Phi))/np.mean(np.absolute(Phi)))
+    np.mean(np.abs(get_Phi_by_FFT(W_fft)-Phi))/np.mean(np.absolute(Phi)))
 
 print()
 print('test of fast_solve')
