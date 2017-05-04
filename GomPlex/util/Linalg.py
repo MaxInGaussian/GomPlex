41c12fb81e287879e08372a411993b897bcc82d0
Max W. Y. Lam <maxingaussian@gmail.com>
2017-05-04 17:28:57 +0800

    commit from max-OptiPlex-7040 by max

--- GomPlex/util/Linalg.py
+++ GomPlex/util/Linalg.py
@@ -28,16 +28,13 @@ def nfft(x, f_hat, M, sigma=2, tol=1e-8):
     mat = csr_matrix((vals.ravel(), col_ind.ravel(), indptr), shape=(len(x), n))
     f = mat.dot(g)
     return f
-
-def ndft_mat(x, F_hat, M):
-    k = -(M//2)+np.arange(M)
-    return np.dot(np.exp(-2j*np.pi*k*x[:, None]), F_hat)
     
-def nfft_mat(x, F_hat, M, sigma=2, tol=1e-8):
-    F = np.zeros((x.shape[0], F_hat.shape[1]))+0j
-    for j in range(F_hat.shape[1]):
-        F[:, j] = nfft(x, F_hat[:, j], M, sigma, tol)
-    return F
+def faster_nfft(n, q, mat, f_hat, M, sigma=2, tol=1e-8):
+    g_hat = f_hat/q
+    g_hat_n = np.concatenate([g_hat[M//2:], np.zeros(n-M), g_hat[:M//2]])
+    g = fftshift(fft(g_hat_n))
+    f = mat.dot(g)
+    return f
 
 def adj_ndft(x, f, M):
     k = -(M//2)+np.arange(M)
@@ -59,35 +56,30 @@ def adj_nfft(x, f, M, sigma=2, tol=1e-8):
     g_hat = n*g_hat_n[(n-M)//2:(n+M)//2]
     f_hat = g_hat/np.exp(-b*(np.pi*k/n)**2)
     return f_hat
-
-def adj_ndft_mat(x, F, M):
-    k = -(M//2)+np.arange(M)
-    return np.dot(np.exp(2j*np.pi*x*k[:, None]), F)
-
-def adj_nfft_mat(x, F, M, sigma=2, tol=1e-8):
-    F_hat = np.zeros((x.shape[0], F.shape[1]))+0j
-    for j in range(F.shape[1]):
-        F_hat[:, j] = adj_nfft(x, F[:, j], M, sigma, tol)
-    return F_hat
+    
+def faster_adj_nfft(n, q, mat, f, M, sigma=2, tol=1e-8):
+    g = mat.T.dot(f)
+    g_hat_n = fftshift(ifft(ifftshift(g)))
+    g_hat = n*g_hat_n[(n-M)//2:(n+M)//2]
+    f_hat = g_hat/q
+    return f_hat
 
 def numpy_solve_Phi(y, x, M):
-    # f_hat = (Phi^H Phi)^-1 Phi^H y
     k = -(M//2)+np.arange(M)
     Phi = np.exp(-2j*np.pi*k*x[:, None])
     return linalg.solve(Phi.conj().T.dot(Phi), Phi.conj().T.dot(y))
 
-def fast_solve_Phi(y, x, M, tol=1e-8):
-    # f_hat = (Phi^H Phi)^-1 Phi^H y
+def solve_Phi_algo_1(y, x, M, sigma=2, tol=1e-8):
     f_hat = np.random.rand(M)*(1e-1+1e-1j)
-    r = y-nfft(x, f_hat, M)
-    _z = adj_nfft(x, r, M)
+    r = y-nfft(x, f_hat, M, sigma, tol)
+    _z = adj_nfft(x, r, M, sigma, tol)
     p = _z.copy()
     while(True):
-        v = nfft(x, p, M)
+        v = nfft(x, p, M, sigma, tol)
         a = _z.conj().T.dot(_z)/v.conj().T.dot(v)
         f_hat += a*p
         r -= a*v
-        z = adj_nfft(x, r, M)
+        z = adj_nfft(x, r, M, sigma, tol)
         b = z.conj().T.dot(z)/_z.conj().T.dot(_z)
         p = b*p+z
         if(np.abs(z).max() < tol):
@@ -95,18 +87,75 @@ def fast_solve_Phi(y, x, M, tol=1e-8):
         _z = z
     return f_hat
 
-def fast_solve_Phi_H(y, x, M, tol=1e-8):
-    # f_hat = (Phi Phi^H)^-1 Phi y
-    f_hat = np.random.rand(x.shape[0])*(1e-1+1e-1j)
-    r = y-adj_nfft(x, f_hat, M)
-    _z = nfft(x, r, M)
+def solve_Phi_algo_2(y, x, M, sigma=2, tol=1e-8):
+    f_hat = np.random.rand(M)*(1e-1+1e-1j)
+    n = M*sigma
+    m = np.ceil(-np.log(0.25*tol/M)/(np.pi*(1-1/(2*sigma-1))))
+    shift = lambda x:-0.5+(x+0.5)%1
+    k = -(M//2)+np.arange(M)
+    col_ind = np.floor(n*x[:, None]).astype(int)+np.arange(-m, m)
+    b = (2*sigma*m)/((2*sigma-1)*np.pi)
+    q = np.exp(-b*(np.pi*k/n)**2)
+    vals = np.exp(-(n*shift(x[:, None]-col_ind/n))**2/b)/np.sqrt(np.pi*b)
+    col_ind = (col_ind+n//2)%n
+    indptr = np.arange(len(x)+1)*col_ind.shape[1]
+    mat = csr_matrix((vals.ravel(), col_ind.ravel(), indptr), shape=(len(x), n))
+    r = y-faster_nfft(n, q, mat, f_hat, M, sigma, tol)
+    _z = faster_adj_nfft(n, q, mat, r, M, sigma, tol)
     p = _z.copy()
     while(True):
-        v = adj_nfft(x, p, M)
+        v = faster_nfft(n, q, mat, p, M, sigma, tol)
         a = _z.conj().T.dot(_z)/v.conj().T.dot(v)
         f_hat += a*p
         r -= a*v
-        z = nfft(x, r, M)
+        z = faster_adj_nfft(n, q, mat, r, M, sigma, tol)
+        b = z.conj().T.dot(z)/_z.conj().T.dot(_z)
+        p = b*p+z
+        if(np.abs(z).max() < tol):
+            break
+        _z = z
+    return f_hat
+
+def solve_Phi_H_algo_1(y, x, M, sigma=2, tol=1e-8):
+    f_hat = np.random.rand(x.shape[0])*(1e-1+1e-1j)
+    r = y-adj_nfft(x, f_hat, M, sigma, tol)
+    _z = nfft(x, r, M, sigma, tol)
+    p = _z.copy()
+    while(True):
+        v = adj_nfft(x, p, M, sigma, tol)
+        a = _z.conj().T.dot(_z)/v.conj().T.dot(v)
+        f_hat += a*p
+        r -= a*v
+        z = nfft(x, r, M, sigma, tol)
+        b = z.conj().T.dot(z)/_z.conj().T.dot(_z)
+        p = b*p+z
+        if(np.abs(z).max() < tol):
+            break
+        _z = z
+    return f_hat
+
+def solve_Phi_H_algo_2(y, x, M, sigma=2, tol=1e-8):
+    f_hat = np.random.rand(x.shape[0])*(1e-1+1e-1j)
+    n = M*sigma
+    m = np.ceil(-np.log(0.25*tol/M)/(np.pi*(1-1/(2*sigma-1))))
+    shift = lambda x:-0.5+(x+0.5)%1
+    k = -(M//2)+np.arange(M)
+    col_ind = np.floor(n*x[:, None]).astype(int)+np.arange(-m, m)
+    b = (2*sigma*m)/((2*sigma-1)*np.pi)
+    q = np.exp(-b*(np.pi*k/n)**2)
+    vals = np.exp(-(n*shift(x[:, None]-col_ind/n))**2/b)/np.sqrt(np.pi*b)
+    col_ind = (col_ind+n//2)%n
+    indptr = np.arange(len(x)+1)*col_ind.shape[1]
+    mat = csr_matrix((vals.ravel(), col_ind.ravel(), indptr), shape=(len(x), n))
+    r = y-faster_adj_nfft(n, q, mat, f_hat, M, sigma, tol)
+    _z = faster_nfft(n, q, mat, r, M, sigma, tol)
+    p = _z.copy()
+    while(True):
+        v = faster_adj_nfft(n, q, mat, p, M, sigma, tol)
+        a = _z.conj().T.dot(_z)/v.conj().T.dot(v)
+        f_hat += a*p
+        r -= a*v
+        z = faster_nfft(n, q, mat, r, M, sigma, tol)
         b = z.conj().T.dot(z)/_z.conj().T.dot(_z)
         p = b*p+z
         if(np.abs(z).max() < tol):
@@ -119,18 +168,26 @@ def numpy_solve_A_tilde(y, x, M, tol=1e-8):
     Phi = np.exp(-2j*np.pi*k*x[:, None])
     return linalg.solve(Phi.conj().T.dot(Phi), y)
 
-def fast_solve_A_tilde(y, x, M, tol=1e-8):
-    return fast_solve_Phi(fast_solve_Phi_H(y, x, M), x, M)
+def solve_A_tilde_algo_1(y, x, M, tol=1e-8):
+    return solve_Phi_algo_1(solve_Phi_H_algo_1(y, x, M), x, M)
 
-def numpy_solve_noisy_nfft(y, x, M, noise, tol=1e-8):
+def solve_A_tilde_algo_2(y, x, M, tol=1e-8):
+    return solve_Phi_algo_2(solve_Phi_H_algo_2(y, x, M), x, M)
+
+def numpy_solve_A(y, x, M, noise, tol=1e-8):
     k = -(M//2)+np.arange(M)
     Phi = np.exp(-2j*np.pi*k*x[:, None])
     A_tilde = Phi.conj().T.dot(Phi)
     return linalg.solve(A_tilde+noise*np.eye(M), Phi.conj().T.dot(y))
 
-def fast_solve_noisy_nfft(y, x, M, noise, tol=1e-8):
-    f_hat = fast_solve_Phi(y, x, M)
-    f_hat -= noise*fast_solve_A_tilde(f_hat, x, M)
+def solve_A_algo_1(y, x, M, noise, tol=1e-8):
+    f_hat = solve_Phi_algo_1(y, x, M)
+    f_hat -= noise*solve_A_tilde_algo_1(f_hat, x, M)
+    return f_hat
+
+def solve_A_algo_2(y, x, M, noise, tol=1e-8):
+    f_hat = solve_Phi_algo_2(y, x, M)
+    f_hat -= noise*solve_A_tilde_algo_2(f_hat, x, M)
     return f_hat
 
 def Phi(X, spectral_freqs):
