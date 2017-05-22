8a3677dd5f3dd53fb4589173bcc7eab858fec99d
Max W. Y. Lam <maxingaussian@gmail.com>
2017-05-22 15:01:45 +0800

    commit from max-OptiPlex-7040 by max

--- applications/digital_drawing/evaluate_features.py
+++ applications/digital_drawing/evaluate_features.py
@@ -9,7 +9,8 @@ from FeatureLearner import FeatureLearner
 
 DRAWING_RAW_DATA_PATH = 'drawing_raw_data.csv'
 
-model = FeatureLearner(use_gender=True, use_age=True, use_edu_level=True,
+model = FeatureLearner(sample_time=200, use_past=10,
+    use_gender=True, use_age=True, use_edu_level=True,
     show_training_drawings=False, show_predicted_drawings=False)
 model.load_drawing_data(DRAWING_RAW_DATA_PATH)
 model.eval_features_for_subjects()
