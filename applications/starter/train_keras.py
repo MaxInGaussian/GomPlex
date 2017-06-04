e47fd9676e4a78a18e0ca18f551b5d8523422baf
Max W. Y. Lam <maxingaussian@gmail.com>
2017-06-04 18:45:16 +0800

    commit from Maxs-Research-Lab.local by MaxLam

--- applications/starter/train_keras.py
+++ applications/starter/train_keras.py
@@ -1,22 +1,21 @@
 import numpy as np
 import numpy.random as npr
 import pandas as pd
+import pickle
 import gc
 
-from sys import path
-path.append("../../")
-from GomPlex import *
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Dropout
+from keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.cross_validation import cross_val_score
 
 print('Loading data ...')
 
 p, t = 0.1, 0.3
-iter_tol = 30
-ratio = 0.3
+nb_epoch = 1000
+batch_size = 100
 cv_folds = 3
-score_rerun = 20
-model_path = 'best.pkl'
-plot_error = False
-metric = Metric('mae')
+model_path = 'best_keras.pkl'
 
 train = pd.read_csv('train2016.csv')
 prop = pd.read_csv('prop2016.csv')
@@ -64,9 +63,8 @@ print('Generating training data ...')
 df_train = train.merge(df_data, how='left', on='parcelid')
 y_train = df_train['logerror'].values
 y_train = np.sign(y_train)*(np.abs(y_train)**p)
-y_train_r = np.maximum(0, y_train)
-y_train_i = np.maximum(0, -y_train)
-y_train = (y_train_r+1j*y_train_i)[:, None]
+y_train = np.hstack(np.maximum(0, y_train)[:, None],
+    np.maximum(0, -y_train)[:, None])
 X_train = df_train.drop(['parcelid', 'logerror', 'transactiondate', 'propertyzoningdesc', 'propertycountylandusecode', 'censustractandblock',
 'rawcensustractandblock'], axis=1)
 columns = X_train.columns
@@ -96,37 +94,62 @@ print('  Gathered %d Training Examples.'%(X_train.shape[0]))
 print('  Gathered %d Testing Examples.'%(X_test.shape[0]))
 print('  Done.')
 
+def save(regressor, path=model_path):
+    with open(path, "wb") as save_f:
+        pickle.dump(regressor, save_f, pickle.HIGHEST_PROTOCOL)
+
+def load(path=model_path):
+    with open(path, "rb") as load_f:
+        return pickle.load(load_f)
+
+def eval(p, y):
+    return np.abs(p[:, 0]-p[:, 1]-y[:, 0]+y[:, 1]).mean()
+
+def model():
+    model = Sequential()
+    model.add(Dense(128, input_dim=len(columns), init='normal'))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.15))
+    model.add(Dense(128, init='normal'))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.15))
+    model.add(Dense(2, init='normal'))
+    model.compile(loss='mae', optimizer='adam')
+    return model
+
 while (True):
-    print('# Training GomPlex')
-    gp = GomPlex(npr.randint(int(np.log(X_train.shape[0]))*3)+8, True)
-    gp.fit(X_train, y_train, cost_type=metric.metric,
-        iter_tol=iter_tol, cv_folds=cv_folds, plot=plot_error)
+    print('# Training Keras')
+    kr = KerasRegressor(build_fn=model,
+        nb_epoch=nb_epoch, batch_size=batch_size, verbose=1)
+    scores = cross_val_score(kr, X_train, y_train, cv=cv_folds)
     print('  Done.')
     print('# Choosing GomPlex Models')
-    score = metric.eval(y_valid, *gp.predict(X_valid))
+    print("msle = %4.2f std = %4.2f" % (scores.mean(),scores.std()))
+    p_valid = kr.model.predict(X_valid)
+    score = eval(p_valid, y_valid)
     print('  new score = %.3f'%(score))
     if(not os.path.exists(model_path)):
-        gp.save(model_path)
+        save(kr)
     else:
-        best_gp = GomPlex().load(model_path).fit(X_train, y_train)
-        best_score = metric.eval(y_valid, *best_gp.predict(X_valid))
+        best_kr = load().fit(X_train, y_train)
+        p_valid = kr.model.predict(X_valid)
+        best_score = eval(p_valid, y_valid)
         print('  best score = %.3f'%(best_score))
         if(score > best_score):
-            gp.save(model_path)
-            backup_path = 'save_models/%s_%.6f.pkl'%(
-                best_gp.hashed_name, best_score)
-            best_gp.save(backup_path)
+            save(kr)
+            backup_path = 'save_models/%.6f.pkl'%(best_score)
+            save(best_kr, backup_path)
             print('  Found New Model!')
     
             print("Start prediction ...")
             test_dates = [288, 319, 349, 653, 684, 714]
             for i, test_date in enumerate(test_dates):
                 X_test[:, -1] = test_date
-                y_test = gp.predict(X_test)[0].ravel()
-                result[result.columns[i+1]] = y_test.real-y_test.imag
+                y_test = kr.predict(X_test)
+                result[result.columns[i+1]] = y_test[:, 0]-y_test[:, 1]
             
             print("Start write result ...")
-            result.to_csv(gp.hashed_name+'.csv', index=False, float_format='%.6f')
+            result.to_csv('%.6f.csv'%(score), index=False, float_format='%.6f')
 
 
 
