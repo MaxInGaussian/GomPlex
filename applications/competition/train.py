806d872872ea8cc9c8237690cb35a9e27893e94d
Max W. Y. Lam <maxingaussian@gmail.com>
2018-01-07 03:54:50 +0800

    try com

--- applications/competition/train.py
+++ applications/competition/train.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 import numpy as np
 import numpy.random as npr
 import pandas as pd
@@ -9,14 +10,18 @@ from GomPlex import *
 
 print('Loading data ...')
 
-p, t = 0.1, 0.3
-iter_tol = 30
-diff_tol = 1e-5
-ratio = 0.25
-cv_folds = 3
 model_path = 'best.pkl'
-plot_error = False
-metric = Metric('mse')
+
+parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+parser.add_argument('--iter_tol', type=int, default=30)
+parser.add_argument('--diff_tol', type=float, default=1e-5)
+parser.add_argument('--cv_folds', type=int, default=3)
+parser.add_argument('--metric', type=str, default='rmse')
+args = parser.parse_args()
+metric = Metric(args.metric)
+iter_tol = args.iter_tol
+diff_tol = args.diff_tol
+cv_folds = args.cv_folds
 
 train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
@@ -35,31 +40,31 @@ y_train = y_train[:, 0][:, None]+y_train[:, 1][:, None]*1j
 
 print("Start training ...")
 
-valid_size = int(X_train.shape[0]*t)
-id_valid = np.random.choice(range(X_train.shape[0]), valid_size)
-id_train = np.setdiff1d(range(X_train.shape[0]), id_valid)
-X_train, y_train, X_valid, y_valid =\
-    X_train[id_train], y_train[id_train], X_train[id_valid], y_train[id_valid]
-
 print('  Gathered %d Training Examples.'%(X_train.shape[0]))
-print('  Gathered %d Validation Examples.'%(X_valid.shape[0]))
 print('  Gathered %d Testing Examples.'%(X_test.shape[0]))
 print('  Done.')
 
 while (True):
     print('# Training GomPlex')
     gp = GomPlex(npr.randint(np.log(X_train.shape[0])**2)+20, True)
+    print('  Chose %d Trigonometric Basis.'%(gp.M))
     gp.fit(X_train, y_train, cost_type=metric.metric,
         iter_tol=iter_tol, diff_tol=diff_tol, cv_folds=cv_folds, plot=False)
     print('  Done.')
     print('# Choosing GomPlex Models')
-    score = metric.eval(y_valid, *gp.predict(X_valid))
+    score = gp.get_cv_metric(cv_folds, 'rmse')
     print('  new score = %.8f'%(score))
     if(not os.path.exists(model_path)):
         gp.save(model_path)
+        y_pred = gp.predict(X_test)[0].ravel()
+        sub = pd.DataFrame()
+        sub["id"] = id_test
+        sub["formation_energy_ev_natom"] = np.exp(y_pred.real)-1
+        sub["bandgap_energy_ev"] = np.exp(y_pred.imag)-1
+        sub.to_csv(str(score)+"_.csv", index=False)
     else:
         best_gp = GomPlex().load(model_path).fit(X_train, y_train)
-        best_score = metric.eval(y_valid, *best_gp.predict(X_valid))
+        best_score = best_gp.get_cv_metric(cv_folds, 'rmse')
         print('  best score = %.8f'%(best_score))
         if(score < best_score):
             gp.save(model_path)
@@ -78,13 +83,6 @@ while (True):
             sub["bandgap_energy_ev"] = np.exp(y_pred.imag)-1
             sub.to_csv(str(score)+"_.csv", index=False)
             
-best_gp = GomPlex().load(model_path).fit(X_train, y_train)
-y_pred = best_gp.predict(X_test)[0].ravel()
-sub = pd.DataFrame()
-sub["id"] = id_test
-sub["formation_energy_ev_natom"] = np.exp(y_pred.real)-1
-sub["bandgap_energy_ev"] = np.exp(y_pred.imag)-1
-sub.to_csv(str(score)+"_.csv", index=False)
 
 
 
